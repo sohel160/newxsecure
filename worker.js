1de7212097589709239e8e879bd22249bb1760be
sohel160 <sohelcmc53rd@gmail.com>
2026-08-31 12:08:41 +0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -36,57 +36,7 @@ proxies:
   - name: "HTTP-1"
     type: http
     server: 103.84.39.92
-    port: 3258
-
-  - name: "HTTP-2"
-    type: http
-    server: 103.84.39.95
-    port: 3258
-
-  - name: "HTTP-3"
-    type: http
-    server: 103.84.39.94
-    port: 3258
-
-  - name: "HTTP-4"
-    type: http
-    server: 103.84.39.113
-    port: 3258
-
-  - name: "HTTP-5"
-    type: http
-    server: 103.84.39.161
-    port: 6258
-
-  - name: "HTTP-6"
-    type: http
-    server: 103.84.36.225
-    port: 52148
-
-  - name: "HTTP-7"
-    type: http
-    server: 103.84.36.237
-    port: 22622
-
-  - name: "HTTP-10"
-    type: http
-    server: 103.84.39.93
-    port: 3258
-
-  - name: "HTTP-11"
-    type: http
-    server: 103.192.159.17
-    port: 6258
-
-  - name: "HTTP-12"
-    type: http
-    server: 113.212.109.12
-    port: 6258
-
-  - name: "HTTP-13"
-    type: http
-    server: 113.212.109.193
-    port: 6258
+    port: 
 
 
 
